2914fe7c6b38cb38c67d1c36d15cd60efe8bc559
Galito-18 <238720115+galoprieto18-gif@users.noreply.github.com>
2026-04-15 12:37:39 -0500

    Ejercicios

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
@@ -14,8 +17,56 @@ public class App {
         }
 
 
+        // TRABAJO DE HOY - GENERAR LISTA DE ESTUDIANTES  Y BUSCAR UN ESTUDIANTE POR SU CEDULA 
 
 
+        int cantidadEstudiantes = 60_000_000;
+        int cantidadConsultas = 40_000;
 
+        System.out.println("Generando estudiantes ...");
+    
+        List<Estudiante>  estudiantes = Generador. generarEstudiantes(cantidadEstudiantes);
+            // es como decir [] est1. est2. est3....
+            //  for (Estudiante est : estudiantes){
+            //     System.out.println(est.nombre);
+            // }
+            //buscar si existe un estudiante con la cedula "51_999_993"
+            //1 forma de buscar un estudiante "lineal"} 
+            String cedulaBuscar = "51_999_993";
+                for(Estudiante est : estudiantes){
+                    if (est.cedula.equals(cedulaBuscar)){
+                        System.out.println("EXISTE");
+                        break;
+                    }
+                }
+            // int inicio = 0;
+            // int fin = estudiantes.size() - 1;
+
+            // while (inicio <= fin) {
+            //     int medio = (inicio + fin) / 2;
+            //     Estudiante actual = estudiantes.get(medio);
+
+            //     int comparacion = actual.cedula.compareTo(cedulaBuscar);
+
+            //     if (comparacion == 0) {
+            //         System.out.println("EXISTE");
+            //         break;
+            //     } else if (comparacion < 0) {
+            //         inicio = medio + 1;
+            //     } else {
+            //         fin = medio - 1;
+            //     }
+            // }
     }
 }
+    
+
+            
+    
+
+
+
+    
+        
+
+
---
+++ src/Generador.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Generador {    
+    public static List<Estudiante> generarEstudiantes(int cantidadEstudiantes){
+        List<Estudiante> estudiantes = new ArrayList<>();
+        for (int i = 0 ; i<cantidadEstudiantes; i++){
+            String cedula = i + ""; // i = 0 -> cedula = "0"
+            String nombre = "Estudiante_" +i; // "Estuiante_8"
+            Estudiante estudiante = new Estudiante(nombre, cedula);
+            estudiantes.add(estudiante);
+   
+        }  
+        return estudiantes;
+    }
+}
+    
+
+
+    
+
